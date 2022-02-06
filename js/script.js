81f8770c9f2d1893498be3260e0d942fdd4784c6
NABIL ATTIA <attia.nabil@hotmail.com>
2022-02-06 17:36:23 +0100

    My website

--- js/script.js
+++ js/script.js
@@ -5,3 +5,24 @@ var typed = new Typed(".typing",{
     BackSpeed:60,
     loop:true
 })
+
+var typed = new Typed(".typing_2",{
+    strings:["","1337 Future Is Loading (42Network)."],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+var typed = new Typed(".typing_3",{
+    strings:["","I'M VERY RESPOSIVE TO MESSAGES"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
+
+var typed = new Typed(".typing_4",{
+    strings:["","I'M AT YOUR SERVICES"],
+    typeSpeed:100,
+    BackSpeed:60,
+    loop:true
+})
